52d2d64452b8457f8d580f19b47826f17db01d8c
guilherme-hs <guilhermbh@gmail.com>
2015-07-16 10:20:58 -0300

    Added Agent Domain Object

---
+++ src/main/java/com/tenacity/pysix/call/agent.java
@@ -0,0 +1,12 @@
+package com.tenacity.pysix.call;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by guilherme on 7/16/15.
+ */
+@Entity
+@Table(name = "${asterisk.agentTable}")
+public class agent {
+}
