0e0c64931b02a10837bb3371787910bfdf4946f4
guilherme-hs <guilhermbh@gmail.com>
2015-12-17 10:01:18 -0200

    feat(license): enabled lombok and created additional manipulation methods

--- src/main/java/com/tenacity/pysix/domain/Customer.java
+++ src/main/java/com/tenacity/pysix/domain/Customer.java
@@ -3,6 +3,7 @@ package com.tenacity.pysix.domain;
 import com.tenacity.pysix.domain.license.License;
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,6 +16,14 @@ public class Customer {
 
     protected String name;
 
-    protected Map<String,License> licenses;
+    protected Map<String,License> licenses = new HashMap<>();
+
+    protected boolean hasLicense(String licenseName){
+        return licenses.containsKey(licenseName);
+    }
+
+    protected License getLicense(String licenseName){
+        return licenses.get(licenseName);
+    }
 
 }
