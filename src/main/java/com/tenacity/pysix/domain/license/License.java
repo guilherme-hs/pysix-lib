2f4fca36a7d8954c3824edca4e85ee8535a35bdc
guilherme-hs <guilhermbh@gmail.com>
2015-12-17 09:45:29 -0200

    refactor(license): enabled lombok and created adtional constructor

--- src/main/java/com/tenacity/pysix/domain/license/License.java
+++ src/main/java/com/tenacity/pysix/domain/license/License.java
@@ -1,12 +1,19 @@
 package com.tenacity.pysix.domain.license;
 
-import com.google.common.base.Objects;
+import lombok.Data;
 
 /**
  * Represents an license
  */
+@Data
 public class License {
-    public License(String name, long validUntil, int quantity) {
+    public License(String name, int quantity) {
+        this.name = name;
+        this.validUntil = 0;
+        this.quantity = quantity;
+    }
+
+    public License(String name, int quantity, long validUntil) {
         this.name = name;
         this.validUntil = validUntil;
         this.quantity = quantity;
@@ -17,40 +24,4 @@ public class License {
     protected long validUntil;
 
     protected int quantity;
-
-    public String getName() {
-        return name;
-    }
-
-    public long getValidUntil() {
-        return validUntil;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof License)) return false;
-        License license = (License) o;
-        return getValidUntil() == license.getValidUntil() &&
-                getQuantity() == license.getQuantity() &&
-                Objects.equal(getName(), license.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getName(), getValidUntil(), getQuantity());
-    }
-
-    @Override
-    public String toString() {
-        return Objects.toStringHelper(this)
-                .add("name", name)
-                .add("validUntil", validUntil)
-                .add("quantity", quantity)
-                .toString();
-    }
 }
