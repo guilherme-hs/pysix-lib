8d125b06807bb430ccb53d2f445ef7ba28d4749d
guilherme-hs <guilhermbh@gmail.com>
2015-12-17 08:59:35 -0200

    refactor(Customer): enabled lombok

--- src/main/java/com/tenacity/pysix/domain/Customer.java
+++ src/main/java/com/tenacity/pysix/domain/Customer.java
@@ -1,12 +1,14 @@
 package com.tenacity.pysix.domain;
 
 import com.tenacity.pysix.domain.license.License;
+import lombok.Data;
 
 import java.util.Map;
 
 /**
  * Represents a Customer
  */
+@Data
 public class Customer {
 
     protected String id;
